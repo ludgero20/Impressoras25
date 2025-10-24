1f4dfda32b6bf8dc01f53a8ce79f219277b09475
Luri Mussi <lurimussi@gmail.com>
2025-10-24 10:22:04 -0300

    ajuste vite

--- vite.config.ts
+++ vite.config.ts
@@ -36,5 +36,13 @@ export default defineConfig({
       strict: true,
       deny: ["**/.*"],
     },
+    // Configuração do proxy para o backend Express
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001', // Aponta para o seu servidor Express
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
 });
